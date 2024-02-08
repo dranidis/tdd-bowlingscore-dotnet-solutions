691385ac4ad3432477e0f10f20d8158ee031d8d2
D Dranidis <dranidis@gmail.com>
2024-02-08 08:32:23 +0200

    Pass one roll

--- src/BowlingScore/Bowling.cs
+++ src/BowlingScore/Bowling.cs
@@ -1,10 +1,19 @@
+using System.Runtime.InteropServices;
+
 namespace BowlingScore
 {
     public class Bowling
     {
+        private readonly List<int> rolls = [];
+
+        public void Roll(int pins)
+        {
+            rolls.Add(pins);
+        }
+
         public int GetScore()
         {
-            return 0;
+            return rolls.FirstOrDefault();
         }
     }
 }
--- test/UnitTests.BowlingScore/BowlingTests.cs
+++ test/UnitTests.BowlingScore/BowlingTests.cs
@@ -14,4 +14,18 @@ public class BowlingTests
 
         Assert.AreEqual(0, score);
     }
+
+    [TestMethod]
+    public void Given_OneRollOf5_GetScore_Returns_5()
+    {
+        // Arrange
+        Bowling bowling = new Bowling();
+
+        // Act
+        bowling.Roll(5);
+        var score = bowling.GetScore();
+
+        // Assert
+        Assert.AreEqual(5, score);
+    }
 }
